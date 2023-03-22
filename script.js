7b6dd9c1155b3dbe2b80212aabc436c145183a17
Samy <s_rahim@estin.dz>
2023-03-22 17:58:05 +0100

    dark modr

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-console.log("hello")
 
 const darkswitch = document.querySelector(".darkswitch")
+const addBtn = document.querySelector(".add-btn")
+const front = document.querySelector(".front")
+const ajoutPopup = document.querySelector(".ajout-popup")
+const blur = document.querySelector(".blur")
+const confirmBtn = document.querySelector(".conrimer-ajout")
+
 
 darkswitch.addEventListener("click",()=>{
         darkswitch.classList.toggle("lightswitch")
+        document.body.classList.toggle("dark")
 
 })
 
-const addBtn = document.querySelector(".add-btn")
-const blur = document.querySelector(".blur")
+
+
+confirmBtn.addEventListener("click", () => {
+    front.classList.toggle("d-none")
+    ajoutPopup.classList.toggle("d-none")
+
+})
 
 
 blur.addEventListener("click", () => {
-    blur.setAttribute("class","blur-disabled")
+    front.classList.toggle("d-none")
+    ajoutPopup.classList.toggle("d-none")
+
 })
 
 document.addEventListener("keydown", (e) => {
     if(e.key == "Escape"){
-        blur.setAttribute("class","blur-disabled")
+        front.classList.toggle("d-none")
+        ajoutPopup.classList.toggle("d-none")
+        console.log("escape")
 
     }
 })
 
 addBtn.addEventListener("click", () => {
-    blur.setAttribute("class","blur blur-active")
+    front.classList.toggle("d-none")
+    ajoutPopup.classList.toggle("d-none")
+    console.log("button click")
+
 })
 
 
