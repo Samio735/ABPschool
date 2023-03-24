84666107303f79fb6344a8026efc1deccea3645a
Samy <s_rahim@estin.dz>
2023-03-24 15:16:26 +0100

    select all

--- script.js
+++ script.js
@@ -13,20 +13,24 @@ darkswitch.addEventListener("click",()=>{
 
 })
 
+if(confirmBtn){
+    confirmBtn.addEventListener("click", () => {
+    front.classList.toggle("d-none")
+    ajoutPopup.classList.toggle("d-none")
+
+})
+}
 
 
-confirmBtn.addEventListener("click", () => {
+
+if(blur){
+    blur.addEventListener("click", () => {
     front.classList.toggle("d-none")
     ajoutPopup.classList.toggle("d-none")
 
 })
 
-
-blur.addEventListener("click", () => {
-    front.classList.toggle("d-none")
-    ajoutPopup.classList.toggle("d-none")
-
-})
+}
 
 document.addEventListener("keydown", (e) => {
     if(e.key == "Escape"){
@@ -37,13 +41,41 @@ document.addEventListener("keydown", (e) => {
     }
 })
 
-addBtn.addEventListener("click", () => {
+if(addBtn){
+    addBtn.addEventListener("click", () => {
     front.classList.toggle("d-none")
     ajoutPopup.classList.toggle("d-none")
     console.log("button click")
 
 })
 
+}
+
+
+
+
+const selectAll = document.querySelector('#selectAll');
+const checkboxes = document.querySelectorAll('#defaultCheck1');
+
+if(selectAll){
+    selectAll.addEventListener('click', function() {
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].checked = selectAll.checked;
+  }
+});
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener('click', function() {
+    let allChecked = true;
+    for (let j = 0; j < checkboxes.length; j++) {
+      if (!checkboxes[j].checked) {
+        allChecked = false;
+        break;
+      }
+    }
+    selectAll.checked = allChecked;
+  });
+}
+}
 
 
 
